c5bc2647031cdce3426c02981cfb5ad3b67595a0
Memphiz <memphis@machzwo.de>
2015-12-21 11:32:14 +0100

    [platform/tvos] - added platform implementations for TVOS from MrMC - TODO UPDATE PLACEHOLDER ART/LOGO IN ASSETS

--- xbmc/Application.cpp
+++ xbmc/Application.cpp
@@ -3628,6 +3628,9 @@ void CApplication::OnPlayBackResumed()
   CXBMCApp::OnPlayBackResumed();
   if (!m_pPlayer->IsPlayingAudio())
     CDarwinUtils::EnableOSScreenSaver(false);
+#elif defined(TARGET_DARWIN_TVOS)
+  if (!m_pPlayer->IsPlayingAudio())
+    CDarwinUtils::EnableOSScreenSaver(false);
 #endif
 
   CVariant param;
